89806391f56bed723571ed35986151e01000ce0f
Muhammad Irfan <111001343+mu-irfan@users.noreply.github.com>
2023-09-14 14:20:46 +0500

    (add:validation): add password and confirm password validation

--- src/schemas/auth.ts
+++ src/schemas/auth.ts
@@ -22,54 +22,59 @@ const loginSchema = z.object({
     }),
 });
 
-const registerSchema = z.object({
-  firstName: z.string().nonempty({
-    message: "First name is required.",
-  }),
-  lastName: z.string().nonempty({
-    message: "Last name is required.",
-  }),
-  email: z
-    .string()
-    .nonempty({
-      message: "Email is required.",
-    })
-    .email({
-      message: "Invalid email.",
+const registerSchema = z
+  .object({
+    firstName: z.string().nonempty({
+      message: "First name is required.",
     }),
-  phone: z
-    .string()
-    .nonempty({
-      message: "Phone is required.",
-    })
-    .min(10, {
-      message: "Phone must be at least 10 characters.",
-    })
-    .max(10, {
-      message: "Phone must be less than 10 characters.",
+    lastName: z.string().nonempty({
+      message: "Last name is required.",
     }),
-  password: z
-    .string()
-    .nonempty({
-      message: "Password is required.",
-    })
-    .min(6, {
-      message: "Password must be at least 6 characters.",
-    })
-    .max(12, {
-      message: "Password must be less than 12 characters.",
-    }),
-  confirmPassword: z
-    .string()
-    .nonempty({
-      message: "Confirm password is required.",
-    })
-    .min(6, {
-      message: "Password must be at least 6 characters.",
-    })
-    .max(12, {
-      message: "Password must be less than 12 characters.",
-    }),
-});
+    email: z
+      .string()
+      .nonempty({
+        message: "Email is required.",
+      })
+      .email({
+        message: "Invalid email.",
+      }),
+    phone: z
+      .string()
+      .nonempty({
+        message: "Phone is required.",
+      })
+      .min(10, {
+        message: "Phone must be at least 10 characters.",
+      })
+      .max(10, {
+        message: "Phone must be less than 10 characters.",
+      }),
+    password: z
+      .string()
+      .nonempty({
+        message: "Password is required.",
+      })
+      .min(6, {
+        message: "Password must be at least 6 characters.",
+      })
+      .max(12, {
+        message: "Password must be less than 12 characters.",
+      }),
+    confirmPassword: z
+      .string()
+      .nonempty({
+        message: "Confirm password is required.",
+      })
+      .min(6, {
+        message: "Password must be at least 6 characters.",
+      })
+      .max(12, {
+        message: "Password must be less than 12 characters.",
+      }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ["confirmPassword"],
+    message: "Password don't match",
+  });
 
 export { loginSchema, registerSchema };
